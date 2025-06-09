214994cda89f3b3c8d480f93bb52719c7d7485ea
Ricardo Canul <ricardocanul7@gmail.com>
2025-06-09 16:02:48 +0200

    Create entities

---
+++ src/main/java/com/ricardocanul/blog/domain/PostStatus.java
@@ -0,0 +1,5 @@
+package com.ricardocanul.blog.domain;
+
+public enum PostStatus {
+    DRAFT, PUBLISHED
+}
---
+++ src/main/java/com/ricardocanul/blog/domain/entities/Post.java
@@ -0,0 +1,86 @@
+package com.ricardocanul.blog.domain.entities;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+import com.ricardocanul.blog.domain.PostStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "posts")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PostStatus status;
+
+    @Column(nullable = false)
+    private Integer readingTime;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, content, status, readingTime, createdAt, updatedAt);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Post post = (Post) obj;
+        return Objects.equals(id, post.id) && Objects.equals(title, post.title) &&
+                Objects.equals(content, post.content) && Objects.equals(status, post.status) &&
+                Objects.equals(readingTime, post.readingTime) && Objects.equals(createdAt, post.createdAt) &&
+                Objects.equals(updatedAt, post.updatedAt);
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
